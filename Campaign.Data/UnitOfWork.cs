6ffc0205a8135dabbab92c557e44b08812da76e1
Deepak Dinakaran <deepakdinakaran22@gmail.com>
2021-03-25 21:05:17 +0530

    Added generic repository and unitofwork

---
+++ Campaign.Data/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace Campaign.Business
+{
+    using Campaign.Data;
+    using System;
+    using Campaign.Data.Entities;
+    public class UnitOfWork : IDisposable
+    {
+        private CampaignContext context = new CampaignContext();
+        private GenericRepository<Campaign> campaignRepository;
+        public GenericRepository<Campaign> CampaignRepository
+        {
+            get
+            {
+                if (this.campaignRepository == null)
+                {
+                    this.campaignRepository = new GenericRepository<Campaign>(context);
+                }
+                return campaignRepository;
+            }
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
