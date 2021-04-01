ef00023bc8a5ce0c84b9fe8c81041aec8851c2bb
Deepak Dinakaran <deepakdinakaran22@gmail.com>
2021-04-01 13:49:48 +0530

    Added references

--- Campaign.Business/CampaignManager.cs
+++ Campaign.Business/CampaignManager.cs
@@ -7,8 +7,16 @@ namespace Campaign.Business
 {
     public class CampaignManager : ICampaignManager
     {
+        private UnitOfWork unitOfWork;
+        public CampaignManager()
+        {
+            this.unitOfWork = new UnitOfWork();
+
+        }
+
         public List<Models.Campaign> GetAllRecords()
         {
+            var campaigns = unitOfWork.CampaignRepository.Get();
             return null;
         }
     }
